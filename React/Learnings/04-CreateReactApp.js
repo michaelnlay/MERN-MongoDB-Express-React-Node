95c4351868b0302b8166f03d132820d047bb8da8
Michael Lay <michaellay2689@gmail.com>
2022-05-04 19:13:33 -0400

    first JSX

---
+++ React/Learnings/04-CreateReactApp.js
@@ -0,0 +1,6 @@
+/*
+Create React App
+Modern JavaScript projects typically use bundlers to "build" source code into production-ready files. One of the most popular bundlers is called Webpack. Webpack is an amazing tool that allows us to break our apps down into smaller modules and transform them based on tools called loaders. It's definitely a technology worth looking into, and you should spend some time working through the Getting Started tutorial.
+
+However, Webpack is not the focus of this React workshop. We'll instead lean on the opinionated starter kit create-react-app, which uses Webpack under the hood.
+*/
